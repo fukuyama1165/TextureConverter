3a3e3ada61748441e35a4e43c9441e38f3d840a2
k021g1165@g.neec.ac.jp <k021g1165@g.neec.ac.jp>
2023-10-02 11:11:52 +0900

    2_3まで

--- main.cpp
+++ main.cpp
@@ -2,9 +2,14 @@
 #include <cstdlib>
 #include <cstdint>
 
-int32_t main()
+int32_t main(int32_t argc,char* argv[])
 {
-	printf("Hello,World!\n");
+	for (uint16_t i = 0; i < argc; i++)
+	{
+		printf(argv[i]);
+		
+		printf("\n");
+	}
 
 	system("pause");
 	return 0;
